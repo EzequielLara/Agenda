b0fc134f5a3248382654a201f4ecdab770bdb040
Ezk24 <Ezk24@DESKTOP-O4DUUU6>
2018-12-05 23:09:38 +0100

    Ejercicio 7. Creo clase Agenda con sus dos atributos.

---
+++ src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.agenda;
+
+/**
+ *
+ * @author Ezk24
+ */
+public class Agenda {
+    
+    // Ejercicio 7. Crea la clase Agenda con los atributos que se especifican.
+    
+    private static final int MAX_CONTACTOS = 10;
+    private int numContactos;
+    
+}
