791ba03232eee2a0afc24f702858380ebfb62968
Ezk24 <Ezk24@DESKTOP-O4DUUU6>
2018-12-05 01:00:51 +0100

    Ejercicio 3. Creo clase Contacto con sus atributos.

---
+++ src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.agenda;
+
+/**
+ *
+ * @author Ezk24
+ */
+public class Contacto {
+    
+    // Ejercicio 3. Crea la clase Contacto con sus atributos correspondientes.
+    
+    private String nombre, telefono, correo;
+    private static final String ER_TELEFONO = "";
+    private static final String ER_CORREO = "";
+}
