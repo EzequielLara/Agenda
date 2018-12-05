ed931f10ea74291ba7f6b09a14c7469ad4d3b820
Ezk24 <Ezk24@DESKTOP-O4DUUU6>
2018-12-05 22:56:54 +0100

    Ejercicio 6.Implemento el resto de métodos de la clase Contacto.

--- src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
@@ -5,6 +5,7 @@
  */
 package org.iesalandalus.programacion.agenda;
 
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,6 +18,9 @@ public class Contacto {
     // Ejercicio 3. Crea la clase Contacto con sus atributos correspondientes.
     
     private String nombre, telefono, correo;
+    
+    // Las siguientes declaraciones de constantes contendrán los patrones para validar telefono y correo.
+    
     private static final String ER_TELEFONO = "(^[6|9][0-9]{8}$)";
     private static final String ER_CORREO = "/[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/igm";
     
@@ -36,7 +40,7 @@ public class Contacto {
     private void setNombre(String nombre) {
         
         /** Declaro el método como privado ya que no se puede modificar el nombre una vez introducido.
-          * compruebo que la cadena no sea null ni esté vacía. 
+          * Compruebo que la cadena no sea null ni esté vacía. 
           * Empleo el método isEmpty() que devuelve true si lenght es igual a 0*/
         
         if ((nombre==null)||(nombre.isEmpty()== true)){
@@ -115,11 +119,89 @@ public class Contacto {
        *teniendo en cuenta que el método toString nos devolverá una cadena con las iniciales
        *del nombre y encerrado entre corchetes el teléfono y el correo separados por comas.
        *También debes tener en cuenta que un contacto será igual que otro si sus nombres, 
-       *ignorando mayúsculas y minúsculas, son iguales.*/
+       *ignorando mayúsculas y minúsculas, son iguales.
+       */
     
  
+    private String getIniciales(){
+        
+      /** Para extraer las iniciales debo identificar antes el número de palabras introducidas. 
+       * Puede que se ingrese solo el nombre, un nombre compuesto, nombre + un apellido
+       * o nombre y varios apellidos. Una vez identificadas las cadenas tendré que seleccionar
+       * la primera letra de cada cadena e introducirlas como String en una variable a 
+       * la que llamare "iniciales" y será la que devolverá el método*. 
+       * Como las iniciales no se podrán cambiar a lo largo del programa declaro el método como privado
+       * 1º tengo que transformar la cadena nombre en array con el método split.
+       */
+      
+       // Declaro e inicializo variable:
+       
+       String iniciales="";
+       
+       // Lanzo un aviso en el caso de que se solicite iniciales de un nombre null:
+       
+       if(iniciales==null||iniciales.isEmpty()){throw new IllegalArgumentException("Debe especificar antes un nombre para obtener las iniciales");} 
+       
+       //Transformo la cadena nombre en un array:
+      
+       String[] arrayNombre = nombre.split(" ");
+      
+       /** Con el bucle y el método substring estraigo la primera letra de cada palabra
+         * y las voy concatenendo con el método concat()*/
+       
+       for(int i=0; i<arrayNombre.length; i++){
+            
+        iniciales = arrayNombre[i].substring(0);
+        iniciales = iniciales.concat(iniciales);
+        }
+       
+       //Paso todo el contenido a mayúsculas.
+       
+        iniciales=iniciales.toUpperCase();
+        
+    return iniciales;
     
+    }
     
+
+    @Override
+    public String toString() {
+        return "Contacto: " + getIniciales() + " [" + telefono + "," + correo + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 17 * hash + Objects.hashCode(this.nombre);
+        hash = 17 * hash + Objects.hashCode(this.telefono);
+        hash = 17 * hash + Objects.hashCode(this.correo);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Contacto other = (Contacto) obj;
+        // Uso el método toUpperCase para que la comparación no dependa de si se introducen los nombres con mayúsculas o minúsculas.
+        if (!Objects.equals(this.nombre.toUpperCase(), other.nombre.toUpperCase())) {
+            return false;
+        }
+        if (!Objects.equals(this.telefono, other.telefono)) {
+            return false;
+        }
+        if (!Objects.equals(this.correo, other.correo)) {
+            return false;
+        }
+        return true;
+    }
     
     
 }
