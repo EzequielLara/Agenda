e407cfb070345040de487520ed74054667ca3b7a
Ezk24 <Ezk24@DESKTOP-O4DUUU6>
2018-12-06 23:00:09 +0100

    Ejercicio 9. Creo método anadir() de la clase Agenda.

--- src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
@@ -5,6 +5,8 @@
  */
 package org.iesalandalus.programacion.agenda;
 
+import javax.naming.OperationNotSupportedException;
+
 /**
  *
  * @author Ezk24
@@ -46,6 +48,65 @@ public class Agenda {
         System.arraycopy(arrayContactos, 0, contactos, 0, getNumContactos());
         
         return contactos;
+    }
+    
+    /**Ejercicio 9.Crea el método anadir para añadir un contacto a la agenda de forma que ésta
+     * se quede ordenada por orden de inserción y sin que admita contactos repetidos.
+     * Apóyate en los métodos privados buscarPrimerIndiceComprobandoExistencia e IndiceNoSuperaTamano. 
+     * El método debe informar de todos los posibles errores mediante la excepcion OperationNotSupportedException:
+     * ya existe ese contacto, el array está lleno, etc.*/
+    
+    
+    private int buscarPrimerIndiceComprobandoExistencia(Contacto contacto)throws OperationNotSupportedException{
+        
+        int indice=-1;
+        boolean posicionLibre=false;
+        
+        for(int i=0;i<getContactos().length && !posicionLibre;i++)
+        {
+            if (getContactos()[i]==null)
+            {
+                posicionLibre=true;
+                indice=i;
+            }
+            else if (getContactos()[i].equals(contacto))
+            {
+                throw new OperationNotSupportedException("Ya existe un contacto con esos datos.");
+            }               
+        }
+        
+        return indice;
+    }
+    
+    private boolean indiceNoSuperaTamano(int j){
+        
+        if(j>=getContactos().length){
+            return true;
+        }else
+            return false;
+    }
+    
+    public void anadir(Contacto contacto)throws OperationNotSupportedException{
+       int i;
+       
+     //Creamos el contacto:
+     
+        contacto= new Contacto(contacto.getNombre(),contacto.getTelefono(), contacto.getCorreo());
+        
+     //Buscamos donde insertar el contacto en el Array de contactos
+        try
+        {
+            i=buscarPrimerIndiceComprobandoExistencia(contacto);
+        }
+        catch(OperationNotSupportedException e)
+        {
+            throw new OperationNotSupportedException("Ya existe un contacto con esos datos.");
+        }
+        
+        if (i!=-1)
+            getContactos()[i]=contacto;
+        else
+            throw new OperationNotSupportedException("El array de libros está lleno.");
     }
     
 }
