0d2e6b40cd4c13f32bb7ee24254f8b86fdcce28b
Ezk24 <Ezk24@DESKTOP-O4DUUU6>
2018-12-06 19:02:42 +0100

    Ejercicio 8. Creo método constructor y getter de la clase Agenda.

--- src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
@@ -16,4 +16,36 @@ public class Agenda {
     private static final int MAX_CONTACTOS = 10;
     private int numContactos;
     
+    /*Ejercicio 8.Crea el constructor y los métodos get que se especifican. 
+     *El método getContactos devolverá una copia del array de contactos
+     */
+
+    public Agenda(int numContactos) {
+        
+        this.numContactos = numContactos;
+    }
+
+    public int getNumContactos() {
+        
+        return numContactos;
+    }
+    
+    public Contacto[] getContactos(){
+      
+        // Creo un array para la clase Contacto en la que se introducirá un número de contactos indicados en el getNumContactos.
+        
+        Contacto [] arrayContactos=new Contacto[getNumContactos()];
+        
+        
+        /**Para crear otro array copia del primero debo declarar antes otro array de la clase Contacto*/
+        
+        Contacto [] contactos =new Contacto[getNumContactos()];
+        
+        // Para obtener una copia del arrayContactos utilizo el método System.arraycopy():
+        
+        System.arraycopy(arrayContactos, 0, contactos, 0, getNumContactos());
+        
+        return contactos;
+    }
+    
 }
